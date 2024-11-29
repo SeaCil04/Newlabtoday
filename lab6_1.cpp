4e23e18cb839d344f361abaff7adb6cef8f3d2cb
Seacil04 <everest1756@gmail.com>
2024-11-29 16:08:09 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(){
+    int n = 1,even,odd;
+    while(n != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> n;
+    if(n % 2 == 0 and n> 0){
+        even += 1;
+    }else if (n % 2 != 0 and n> 0){
+        odd += 1;
+    }
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
+    
     return 0;
 }
